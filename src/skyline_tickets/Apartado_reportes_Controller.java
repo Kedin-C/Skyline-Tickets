8bc6f245df5a50a538faa53c4d4865d3658c9c69
JUANDA <juansalcedo.perez28@gmail.com>
2026-06-26 18:27:08 -0500

    Subi las cosas que he hecho a github

---
+++ src/skyline_tickets/Apartado_reportes_Controller.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package skyline_tickets;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import View.Apartado_reportes_view;
+//import skyline_tickets.view.TipoReportes;
+import skyline_tickets.view.ApartadoReportes_view;
+
+
+/**
+ *
+ * @author juans
+ */
+public class Apartado_reportes_Controller implements ActionListener{
+    
+    public ApartadoReportes_view vista;
+    public Apartado_reportes_view vistaR;
+    
+    public Apartado_reportes_Controller(ApartadoReportes_view vista, Apartado_reportes_view vistaR){
+        this.vista=vista;
+        this.vistaR=vistaR;
+        this.vista.Rfinanciero.addActionListener(this);
+        this.vista.Roperacional.addActionListener(this);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == vista.Rfinanciero){
+            vista.setVisible(false);
+            vistaR.setVisible(true);
+            vistaR.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
+//            TipoReportes.tipo = 1;
+        }else if(e.getSource() == vista.Roperacional){
+            vista.setVisible(false);
+            vistaR.setVisible(true);
+            vistaR.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
+//            TipoReportes.tipo = 2;
+        }
+    }
+    
+    
+    
+}
---
+++ src/skyline_tickets/Reportes_controlador.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package skyline_tickets;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import Model.ReportesDao;
+import View.Apartado_reportes_view;
+
+/**
+ *
+ * @author juans
+ */
+public class Reportes_controlador implements ActionListener{
+    
+    public ReportesDao persona = new ReportesDao();
+    public Apartado_reportes_view vista = new Apartado_reportes_view();
+    DefaultTableModel modelo = new DefaultTableModel();
+    Reportes_creadorPdf CrearPdf;
+    
+    public Reportes_controlador(Apartado_reportes_view vista, ReportesDao dao) {
+        this.vista = vista;
+        this.persona = dao;
+        this.CrearPdf = new Reportes_creadorPdf();
+        this.vista.btnExportar.addActionListener(this);
+        cargarTabla();
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == vista.btnExportar){
+            CrearPdf.setCrearPdf(vista.tabla);
+        }
+    }
+
+    private void cargarTabla() {
+        List datos = persona.listar();
+        DefaultTableModel modelo = (DefaultTableModel) vista.tabla.getModel();
+        modelo.setRowCount(0);
+
+        for (Object obj : datos) {
+            Object[] fila = (Object[]) obj;
+            modelo.addRow(fila);
+        }
+    }
+    
+}
