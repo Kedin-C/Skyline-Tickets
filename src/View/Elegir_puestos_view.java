92674e08670297489ac1bf1a7ad045c4027c900d
senastianor08 <sebastian.or790@gmail.com>
2026-06-29 17:20:25 -0500

    Las vistas de la compra de un ticket

---
+++ src/View/Elegir_puestos_view.java
@@ -0,0 +1,157 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package skyline_tickets.view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+
+public class Elegir_puestos_view extends Interfaz_vista_abtractas{
+    
+    private JPanel contenedor_principal, puestos, puestosTorre1, puestosTorre2, siguiente_volver; 
+    public JButton torre1[][], torre2[][], siguiente, volver, aleatorio;
+    private FondoPanel avion;
+    private JScrollPane scrol;
+    public Elegir_puestos_view() {
+        
+        String nombrePagina = "Elegir Puestos";
+        super(nombrePagina);
+        contenedor_principal = super.getPanel2();
+        
+        //Agregando la imagen de fondo
+        avion = new FondoPanel("avion.png");
+        avion.setLayout(null);
+        avion.setOpaque(false);
+        
+        //Definiendo las filas y columnas
+        int fila1 = 30, fila2 = 30, columna1 = 3, columna2 = 3;
+        
+        //Creando las matricez de botones
+        torre1 = new JButton[fila1][columna1];
+        torre2 = new JButton[fila2][columna2];
+        
+        //Creando los paneles
+        puestos = new JPanel(new FlowLayout());
+        puestosTorre1 = new JPanel(new GridLayout(fila1,columna1));
+        puestosTorre2 = new JPanel(new GridLayout(fila2,columna2));
+        siguiente_volver = new JPanel(new FlowLayout(FlowLayout.RIGHT,15, 10));
+        
+        //Creando los botones de las 2 matricez de botones
+        for(int f=0; f<fila1; f++){
+            for(int c=0; c<columna1; c++){
+                
+                char letra=(char)('A' + c);
+                String nombreBoton = "" + letra + (f + 1);
+                
+                torre1[f][c] = new JButton(nombreBoton);
+                
+                torre1[f][c].setPreferredSize(new Dimension(49,25));
+                torre1[f][c].setFont(new Font("Arial", Font.BOLD, 8));
+                
+                puestosTorre1.add(torre1[f][c]);
+            }
+        }
+        
+        for(int f=0; f<fila2; f++){
+            for(int c=0; c<columna2; c++){
+                
+                char letra=(char)('D' + c);
+                String nombreBoton = "" + letra + (f + 1);
+                
+                torre2[f][c] = new JButton(nombreBoton);
+                
+                torre2[f][c].setPreferredSize(new Dimension(49,25));
+                torre2[f][c].setFont(new Font("Arial",Font.BOLD, 8));
+                
+                puestosTorre2.add(torre2[f][c]);
+            }
+        }
+        
+        //Para que no tenga fondo
+        puestos.setOpaque(false);
+        
+        
+        //Agregando los 2 paneles con la las 2 matricez de botones
+        puestos.add(puestosTorre1);
+        puestos.add(puestosTorre2);
+        
+        
+        //Scroll para navegar entre los botones
+        scrol = new JScrollPane(puestos);
+        
+        scrol.setOpaque(false);
+        
+        scrol.getViewport().setOpaque(false);
+        
+        scrol.setBorder(null);
+        
+        scrol.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrol.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        
+        //Tamaño y posiscion de donde estan los botones
+        scrol.setBounds(640, 135, 250, 340);
+        
+        //Agregando la matriz de botones para que quede encima de la imagen
+        avion.add(scrol);
+        
+        //Creando y decorando los botones
+        siguiente = new JButton("Siguiente");
+        volver = new JButton("Volver");
+        aleatorio = new JButton("Aleatorio");
+        
+        siguiente.setForeground(Color.WHITE);
+        volver.setForeground(Color.WHITE);
+        aleatorio.setForeground(Color.WHITE);
+        
+        siguiente.setBackground(Color.decode("#037FB9"));
+        volver.setBackground(Color.decode("#037FB9"));
+        aleatorio.setBackground(Color.decode("#037FB9"));
+        
+        //Agregando los botones al panel
+        siguiente_volver.add(volver);
+        siguiente_volver.add(siguiente);
+        siguiente_volver.add(aleatorio);
+        siguiente_volver.setPreferredSize(new Dimension(50,60));
+        
+        //Agregando los componentes al contenedor principal
+        contenedor_principal.setLayout(new BorderLayout());
+        contenedor_principal.add(avion, BorderLayout.CENTER);
+        contenedor_principal.add(siguiente_volver, BorderLayout.SOUTH);
+        
+        
+        this.setSize(550, 800);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+    
+}
+
+//El avion de fondo 
+class FondoPanel extends JPanel {
+    private Image imagen;
+
+    public FondoPanel(String ruta) {
+        this.imagen = new ImageIcon(ruta).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); 
+        if (imagen != null) {
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
---
+++ src/View/Tarjeta_de_credito_view.java
@@ -0,0 +1,220 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package skyline_tickets.view;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import com.toedter.calendar.JDateChooser;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.text.SimpleDateFormat;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPopupMenu;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.border.TitledBorder;
+
+
+public class Tarjeta_de_credito_view extends Interfaz_vista_abtractas{
+    
+    private JPanel contenedor_principal, datos_tarjeta, imagen,formulario, volver_pagar;//contenedor
+    private JButton volver,pagar;
+    private JLabel numeroTarjeta, fechaVen, cv, nombreTitular;
+    private TitledBorder titulo;
+    private JDateChooser fecha_ven;
+    private SimpleDateFormat formatoFecha;
+    private JTextField num_tarjeta, cvv, nombre_titular;
+    
+    
+    public Tarjeta_de_credito_view() {
+        String nombrePagina = "Tarjeta de credito";
+        super(nombrePagina);
+        
+        contenedor_principal = super.getPanel2();
+        contenedor_principal.setLayout(new BorderLayout());
+        
+        //Creando los contenedores 
+        datos_tarjeta = new JPanel(new FlowLayout(FlowLayout.CENTER,30,0));
+        formulario = new JPanel(new GridLayout(4,2,40,15));
+        volver_pagar = new JPanel(new FlowLayout(FlowLayout.RIGHT, 30, 20));
+        
+        
+        //Creando el contenedor con la imagen de la tarjeta
+        imagen = new FondoPanel("Tarjeta_credito.PNG");
+        
+        //Tamaños de la imagen y de el formulario
+        Dimension tamanoFormulario = new Dimension(450, 220);
+        
+        formulario.setPreferredSize(tamanoFormulario);
+        formulario.setMinimumSize(tamanoFormulario);
+        formulario.setMaximumSize(tamanoFormulario);        
+        
+        Dimension tamanoImagen = new Dimension(550, 350);
+        
+        imagen.setPreferredSize(tamanoImagen);
+        imagen.setMinimumSize(tamanoImagen);
+        imagen.setMaximumSize(tamanoImagen); 
+        
+        //Titulo del borde
+        Font fuenteGrande = new Font("Arial", Font.BOLD, 20);
+        titulo = new TitledBorder("Tarjeta de credito");
+        titulo.setTitleFont(fuenteGrande);
+        
+        formulario.setBorder(titulo);
+        
+        
+        //Creando los textos de los campos
+        numeroTarjeta = new JLabel("Numero de tarjeta*");
+        fechaVen = new JLabel("Fecha de vencimiento*");
+        cv = new JLabel("CVV*");
+        nombreTitular = new JLabel("Nombre tirular*");
+        
+        
+        //Creando los campos para ingresar la informacion
+        num_tarjeta = new JTextField(12);
+        cvv = new JTextField(12);
+        nombre_titular = new JTextField(12);
+        
+        fecha_ven = new JDateChooser();
+        
+        formatoFecha = new SimpleDateFormat("dd/mm/yyyy");//Formato de la fecha dia/mes/año
+        
+        
+        //Condicones para que los campos solo permitan siertos caracteres
+        num_tarjeta.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                if (!Character.isDigit(e.getKeyChar()) && e.getKeyChar() != ' ')//Solo numeros y espacios 
+                {
+                    e.consume();
+                }
+            }
+        });
+        
+        //Desactivar el comando de "Pegar" (Ctrl + V)
+        num_tarjeta.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_V, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()), "none");
+
+        //Desactivar el clic derecho
+        num_tarjeta.setComponentPopupMenu(new JPopupMenu());
+        
+        //Para que no pueda ingresar al campo de fecha
+        JTextField editorFecha = (JTextField) fecha_ven.getDateEditor().getUiComponent();
+        editorFecha.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                e.consume();
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // Bloqueamos absolutamente todo: letras, números, Backspace y Suprimir
+                e.consume();
+            }
+        });
+        
+        //Desactivar el comando de "Pegar" (Ctrl + V)
+        fecha_ven.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_V, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()), "none");
+
+        //Desactivar el clic derecho
+        fecha_ven.setComponentPopupMenu(new JPopupMenu());
+        
+        cvv.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                if (!Character.isDigit(e.getKeyChar()))//Solo numeros
+                {
+                    e.consume();
+                }
+            }
+        });
+        
+        //Desactivar el comando de "Pegar" (Ctrl + V)
+        cvv.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_V, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()), "none");
+
+        //Desactivar el clic derecho
+        cvv.setComponentPopupMenu(new JPopupMenu());
+        
+        nombre_titular.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                if (!Character.isLetter(e.getKeyChar()) && e.getKeyChar() != ' ')//Solo letras y espacios
+                {
+                    e.consume();
+                }
+            }
+        });
+        
+        //Desactivar el comando de "Pegar" (Ctrl + V)
+        nombre_titular.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_V, Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()), "none");
+
+        //Desactivar el clic derecho
+        nombre_titular.setComponentPopupMenu(new JPopupMenu());
+        
+        //Agregando los componentes al formulario
+        formulario.add(numeroTarjeta);
+        formulario.add(num_tarjeta);
+        formulario.add(fechaVen);
+        formulario.add(fecha_ven);
+        formulario.add(cv);
+        formulario.add(cvv);
+        formulario.add(nombreTitular);
+        formulario.add(nombre_titular);
+        
+        //Agregando al contenedor para unirlo a la imagen
+        datos_tarjeta.add(formulario, FlowLayout.LEFT);
+        datos_tarjeta.add(imagen, FlowLayout.CENTER);
+        
+        
+        //Creando botones
+        volver = new JButton("Volver");
+        pagar = new JButton("Realizar pago");
+        
+        volver.setForeground(Color.WHITE);
+        pagar.setForeground(Color.WHITE);
+        
+        volver.setBackground(Color.decode("#037FB9"));
+        pagar.setBackground(Color.decode("#037FB9"));
+        
+        volver_pagar.add(volver);
+        volver_pagar.add(pagar);
+        
+        volver_pagar.setPreferredSize(new Dimension(100,80));
+        
+        
+        contenedor_principal.add(datos_tarjeta, BorderLayout.CENTER);
+        contenedor_principal.add(volver_pagar, BorderLayout.SOUTH);
+        
+        this.setSize(550, 800);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    }
+    
+}
+
+//La imagen de la tarjeta
+class FondoPanel extends JPanel {
+    private Image imagen;
+
+    public FondoPanel(String ruta) {
+        this.imagen = new ImageIcon(ruta).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); 
+        if (imagen != null) {
+            g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
