f881d03b4a8cd0eeea105be5806a29505b6eb6ff
Nicolas Bonilla <nikobongru@gmail.com>
2026-06-30 11:27:15 -0500

    Agrega Codigo_descuento y Codigo_descuentoDao

---
+++ src/Model/Codigo_descuentoDao.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Random;
+import model.Conexion;
+import model.Codigo_descuento;
+/**
+ *
+ * @author Nikob
+ */
+public class Codigo_descuentoDao {
+    
+
+    Conexion conexionBD = new Conexion();
+    Connection con;
+
+    public Codigo_descuento generarCodigo() {
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 8; i++) {
+            sb.append(caracteres.charAt(random.nextInt(caracteres.length())));
+        }
+
+        Codigo_descuento codigo = new Codigo_descuento();
+        codigo.setCodigo(sb.toString());
+        codigo.setPorcentajeDescuento(random.nextInt(6) + 10); // entre 10% y 15%
+        return codigo;
+    }
+
+    public boolean guardarCodigo(Codigo_descuento codigo) {
+        String sql = "INSERT INTO codigo_descuento(codigo, porcentaje_descuento, usado) VALUES(?,?,0)";
+        try {
+            con = conexionBD.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, codigo.getCodigo());
+            ps.setInt(2, codigo.getPorcentajeDescuento());
+            ps.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+}
